084d5222236fa0746910ac711eef4f8484504b41
FTS427 <FTS427@outlook.com>
2024-09-17 02:44:52 +0800

    F | fix some places

--- src/endstone_better_server/better_server.py
+++ src/endstone_better_server/better_server.py
@@ -9,11 +9,11 @@ class BetterServer(Plugin):
 
     @event_handler
     def on_player_join(self, event: PlayerJoinEvent):
-        PlayerJoinEvent.join_message = f"§e§l{event.player.name}§r 來此共戲"
+        event.join_message = f"§e§l{event.player.name}§r 來此共戲"
         self.logger.info(f"{event.player.name} {event.player.xuid} using {event.player.device_os}"+ f"at {event.player.location}")
         event.player.send_toast(f"Welcome! §e§l{event.player.name}§r","欢迎回到 §s§lZH-Server§r!")
     
     @event_handler
     def on_player_quit(self, event: PlayerQuitEvent):
-        PlayerQuitEvent.quit_message = f"§e§l{event.player.name}§r 去矣"
+        event.quit_message = f"§e§l{event.player.name}§r 去矣"
         self.logger.info(f"{event.player.name} {event.player.xuid} left")
