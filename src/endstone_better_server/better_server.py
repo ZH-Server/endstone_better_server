f9ebeb63341d4b87abf18f95592050039cc6b0f0
FTS427 <FTS427@outlook.com>
2024-09-16 11:34:36 +0800

    U | delete useless code

--- src/endstone_better_server/better_server.py
+++ src/endstone_better_server/better_server.py
@@ -1,8 +1,6 @@
 from endstone.plugin import Plugin
 from endstone import ColorFormat
 from endstone.event import *
-import time
-
 class BetterServer(Plugin):
     api_version = "0.5"
 
@@ -11,6 +9,5 @@ class BetterServer(Plugin):
 
     @event_handler
     def on_player_join(self, event: PlayerJoinEvent):
-        self.server.broadcast_message(ColorFormat.BOLD + ColorFormat.WHITE + f"{event.player.name}" + ColorFormat.YELLOW + "来啦~")
-        self.logger.info(ColorFormat.BOLD + ColorFormat.WHITE + f"{event.player.name} {event.player.xuid} using {event.player.device_os}"+ ColorFormat.YELLOW + f"{event.player.dimension} {event.player.location}" + ColorFormat.WHITE + "has joined")
-        event.player.send_toast("Welcome!","欢迎回到 ZH-Server!")
+        self.logger.info(ColorFormat.BOLD + ColorFormat.WHITE + f"{event.player.name} {event.player.xuid} using {event.player.device_os}"+ ColorFormat.YELLOW + f" {event.player.location}" + ColorFormat.WHITE + "has joined")
+        event.player.send_toast(f"Welcome! §e§l{event.player.name}§r","欢迎回到 §s§lZH-Server§r!")
