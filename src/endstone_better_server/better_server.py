ce9f1080d963d41659c6b8a1d31598bb885e46c0
FTS427 <FTS427@outlook.com>
2024-10-02 11:02:05 +0800

    awa

--- src/endstone_better_server/better_server.py
+++ src/endstone_better_server/better_server.py
@@ -1,5 +1,4 @@
 from endstone.plugin import Plugin
-from endstone import ColorFormat
 from endstone.event import *
 class BetterServer(Plugin):
     api_version = "0.5"
@@ -10,10 +9,9 @@ class BetterServer(Plugin):
     @event_handler
     def on_player_join(self, event: PlayerJoinEvent):
         event.join_message = f"§e§l{event.player.name}§r 來此共戲"
-        self.logger.info(f"{event.player.name} {event.player.xuid} using {event.player.device_os}"+ f"at {event.player.location}")
+        self.logger.info(f"{event.player.name} {event.player.xuid} {event.player.device_os}"+ f" at {event.player.location}")
         event.player.send_toast(f"Welcome! §e§l{event.player.name}§r","欢迎回到 §s§lZH-Server§r!")
     
     @event_handler
     def on_player_quit(self, event: PlayerQuitEvent):
         event.quit_message = f"§e§l{event.player.name}§r 去矣"
-        self.logger.info(f"{event.player.name} {event.player.xuid} left")
